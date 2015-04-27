b83a7e2a78bd1d5e73151ec6bfffc5a11bae0832
stevenlee <stevenlee87@users.noreply.github.com>
2015-04-27 11:23:49 +0800

    Update settings.py

--- aos/aos/settings.py
+++ aos/aos/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 	    'NAME': 'aos',                      # Or path to database file if using sqlite3.
 		'USER': 'root',                      # Not used with sqlite3.
-		'PASSWORD': 'ledoadmin',                  # Not used with sqlite3.
+		'PASSWORD': 'root',                  # Not used with sqlite3.
 		'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
 		'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -99,10 +99,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/export/www/static/aos/'
+STATIC_ROOT = 'www/static/aos/'
 
 
 TEMPLATE_DIRS = (
-    '/export/www/aos/aos/templates/',
+    'www/aos/aos/templates/',
 )
 
